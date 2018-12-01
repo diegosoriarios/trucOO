34c389e3c59d7642d19db848b309532f70bf29a3
Diego Soria Rios <diegosoriarios@gmail.com>
2018-12-01 20:32:50 -0200

    Só falta testar agora

--- src/teste/Main.java
+++ src/teste/Main.java
@@ -78,115 +78,47 @@ public class Main{
                 int escolha = JOptionPane.showOptionDialog(null, cartasPlayer1 , player1.getNome(), JOptionPane.WARNING_MESSAGE, 0, null, gritos, gritos[0]);
                 switch(escolha){
                     case 0:
+                     /*---------------------ENVIDO----------------------------*/
                      if(j != 0){
                          JOptionPane.showMessageDialog(null, "Escolha uma opção valida!!");
                          j-=2;
                          break;
                      }
                      gritos[0] = "";
-                     String[] envidoOpt = {"Não Quero", "Quero", "Real Envido", "Falta Envido"};
-                     int envido = JOptionPane.showOptionDialog(null, cartasPlayer2, player2.getNome() + " aceita o envido?", JOptionPane.YES_NO_OPTION, 0, null, envidoOpt, envidoOpt[0]);
-                        if(envido == 1){
-                            int envP1 = p.calculaEnvido(player1.listaCarta);
-                            int envP2 = p.calculaEnvido(player2.listaCarta);
-                            System.out.println("Quero");
-                            System.out.println(envP1);
-                            System.out.println(envP2);
-                            int g = p.venceChamada(player1.isMao(), envP1, envP2);
-                            if(g == 1){
-                                p.adicionaPontos(player1, p.verificaPontos("Envido"));
-                            }else{
-                                p.adicionaPontos(player2, p.verificaPontos("Envido"));
-                            }
-                        }else{
-                            if(envido == 2){
-                                int resposta_envido = JOptionPane.showOptionDialog(null, cartasPlayer1, player1.getNome() + " aceita o real envido?", JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
-                                if(resposta_envido == 1){
-                                    int envP1 = p.calculaEnvido(player1.listaCarta);
-                                    int envP2 = p.calculaEnvido(player2.listaCarta);
-                                    int g = p.venceChamada(player1.isMao(), envP1, envP2);
-                                    if(g == 1){
-                                        p.adicionaPontos(player1, p.verificaPontos("RealEnvido"));
-                                    }else{
-                                        p.adicionaPontos(player2, p.verificaPontos("RealEnvido"));
-                                    }
-                                }else{
-                                    p.adicionaPontos(player2, p.verificaPontos("Envido"));
-                                }
-                            }else{
-                                if(envido == 3){
-                                    int resposta_envido = JOptionPane.showOptionDialog(null, cartasPlayer1, player2.getNome() + " aceita a Falta Envido?", JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
-                                    if(resposta_envido == 1){
-                                        int envP1 = p.calculaEnvido(player1.listaCarta);
-                                        int envP2 = p.calculaEnvido(player2.listaCarta);
-                                        int g = p.venceChamada(player1.isMao(), envP1, envP2);
-                                        if(g == 1){
-                                            p.adicionaPontos(player1, p.verificaPontos("FaltaEnvido"));
-                                        }else{
-                                            p.adicionaPontos(player2, p.verificaPontos("FaltaEnvido"));
-                                        }
-                                    }else{
-                                        p.adicionaPontos(player2, p.verificaPontos("Envido"));
-                                    }
-                                }else{
-                                    p.adicionaPontos(player2, p.verificaPontos("Nao"));
-                                }    
-                            }
-                        }
-                        break;
+                     gritos[1] = "";
+                     chamaEnvido(p, player1, player2);
+                     continue;
                     case 1:
+                    /*-------------------------FLOR---------------------------*/
                     if(j != 0){
                         JOptionPane.showMessageDialog(null, "Escolha uma opção valida!!");
                         j-=2;
                         break;
                     }
-                    String[] florOpt = {"É bom...", "Contra Flor", "Flor ao Resto"};
-                    int flor = JOptionPane.showOptionDialog(null, "FLOR", player2.getNome(), JOptionPane.YES_NO_OPTION, 0, null, florOpt, florOpt[0]);
-                        if(flor == 0){
-                            p.adicionaPontos(player1, p.verificaPontos("Flor"));
-                        }else{
-                            if(flor == 1){
-                                int resposta_flor = JOptionPane.showOptionDialog(null, "Aceita a Falta Envido?", player2.getNome(), JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
-                                if(resposta_flor == 0){
-                                    int florP1 = p.calculaFlor(player1.listaCarta);
-                                    int florP2 = p.calculaFlor(player2.listaCarta);
-                                    int g = p.venceChamada(player1.isMao(), florP1, florP2);
-                                    if(g == 1){
-                                        p.adicionaPontos(player1, p.verificaPontos("ContraFlor"));
-                                    }else{
-                                        p.adicionaPontos(player2, p.verificaPontos("ContraFlor"));
-                                    }
-                                }else{
-                                    p.adicionaPontos(player2, p.verificaPontos("Flor"));
-                                }
-                            }else{
-                                int resposta_flor = JOptionPane.showOptionDialog(null, "Aceita a Falta Envido?", player2.getNome(), JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
-                                if(resposta_flor == 0){
-                                    int florP1 = p.calculaFlor(player1.listaCarta);
-                                    int florP2 = p.calculaFlor(player2.listaCarta);
-                                    int g = p.venceChamada(player1.isMao(), florP1, florP2);
-                                    if(g == 1){
-                                        p.adicionaPontos(player1, p.verificaPontos("FlorEoResto"));
-                                    }else{
-                                        p.adicionaPontos(player2, p.verificaPontos("FlorEoResto"));
-                                    }
-                                }else{
-                                    p.verificaPontos("Flor");
-                                    p.adicionaPontos(player2, p.verificaPontos("Flor"));
-                                }
-                            }
-                        }
-                        gritos[1] = "";
-                        break;
+                    chamaFlor(p, player1, player2);
+                    gritos[0] = "";
+                    gritos[1] = "";
+                    continue;
                     case 2:
+                    /*--------------------------TRUCO-------------------------*/
                         if(pontoRodada == 0){
-                            pontoRodada = chamaTruco(p, pontoRodada,player1, player2);   
+                            pontoRodada = chamaTruco(p, pontoRodada,player1, player2);
+                            if(pontoRodada == 0){
+                                j = 5;
+                                continue;
+                            }
                         }else{
                             if(pontoRodada == 1){
                                 pontoRodada = chamaRetruco(p, pontoRodada, player1, player2);
+                                if(pontoRodada == 1){
+                                    j = 5;
+                                    continue;
+                                }
                             }else{
                                 if(pontoRodada == 2){
                                     pontoRodada = chamaValeQuatro(p, pontoRodada, player1, player2);
+                                    j = 5;
+                                    continue;
                                 }else{
                                     JOptionPane.showMessageDialog(null, "Escolha uma opção valida!!");
                                     j-=2;
@@ -197,12 +129,17 @@ public class Main{
                     case 3:
                         break;
                 }
+                /*JOGAR CARTAS */
                 Cartas jc1;
                 Cartas jc2;
                 do{
                     jc1 = jogarCartas(player1);
                 }while(jc1 == null);
-                String[] truco2 = {"Truco", "Soltar Carta"};
+                String[] truco2 = {"Truco", "Soltar Carta", "Envido", "Flor"};
+                if(j != 0){
+                    truco2[2] = "";
+                    truco2[3] = "";
+                }
                 if(pontoRodada == 0){
                     truco2[0] = "Truco";
                 }else{
@@ -222,24 +159,35 @@ public class Main{
                         pontoRodada = chamaTruco(p, pontoRodada, player2, player1);
                         if(pontoRodada == 0){
                             j = 5;
+                            continue;
                         }
                     }else{
                         if(pontoRodada == 1){
                             pontoRodada = chamaRetruco(p, pontoRodada, player2, player1);
                             if(pontoRodada == 1){
                                 j = 5;
+                                continue;
                             }
                         }else{
                             if(pontoRodada == 2){
                                 pontoRodada = chamaValeQuatro(p, pontoRodada, player2, player1);
                                 if(pontoRodada == 2){
                                     j = 5;
+                                    continue;
                                 }
                             }else{
                                 JOptionPane.showMessageDialog(null, "Escolha uma opção valida!!");
                             }
                         }
                     } 
+                }else{
+                    if(t2 == 2){
+                        chamaEnvido(p, player2, player1);
+                    }else{
+                        if(t2 == 3){
+                            chamaFlor(p, player2, player1);
+                        }
+                    }
                 }
                 j--;
                 do{
@@ -251,7 +199,7 @@ public class Main{
                     if(pontoRodada == 0){
                         p.adicionaPontos(player1, p.verificaPontos("Nao"));
                     }else{
-                        if(pontoRodada == 1){
+                        if(pontoRodada  == 1){
                             p.adicionaPontos(player1, p.verificaPontos("Truco"));
                         }else{
                             if(pontoRodada == 2){
@@ -311,6 +259,107 @@ public class Main{
         }
     }
     
+    public static void chamaEnvido(Partida p, Player player1, Player player2){
+        Object[] confirmacao = {"Sim", "Não"};
+        String cartasPlayer1 = player1.listaCarta[0].toString() + player1.listaCarta[1].toString() + player1.listaCarta[2].toString();
+        Object[] cartasPlayer2 = {player2.listaCarta[0] != null ? player2.listaCarta[0].toString() : "", 
+                                  player2.listaCarta[1] != null ? player2.listaCarta[1].toString() : "", 
+                                  player2.listaCarta[2] != null ? player2.listaCarta[2].toString() : ""};
+        String[] envidoOpt = {"Não Quero", "Quero", "Real Envido", "Falta Envido"};
+        int envido = JOptionPane.showOptionDialog(null, cartasPlayer2, player2.getNome() + " aceita o envido?", JOptionPane.YES_NO_OPTION, 0, null, envidoOpt, envidoOpt[0]);
+           if(envido == 1){
+               int envP1 = p.calculaEnvido(player1.listaCarta);
+               int envP2 = p.calculaEnvido(player2.listaCarta);
+               System.out.println("Quero");
+               System.out.println(envP1);
+               System.out.println(envP2);
+               int g = p.venceChamada(player1.isMao(), envP1, envP2);
+               if(g == 1){
+                   p.adicionaPontos(player1, p.verificaPontos("Envido"));
+               }else{
+                   p.adicionaPontos(player2, p.verificaPontos("Envido"));
+               }
+           }else{
+               if(envido == 2){
+                   int resposta_envido = JOptionPane.showOptionDialog(null, cartasPlayer1, player1.getNome() + " aceita o real envido?", JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
+                   if(resposta_envido == 1){
+                       int envP1 = p.calculaEnvido(player1.listaCarta);
+                       int envP2 = p.calculaEnvido(player2.listaCarta);
+                       int g = p.venceChamada(player1.isMao(), envP1, envP2);
+                       if(g == 1){
+                           p.adicionaPontos(player1, p.verificaPontos("RealEnvido"));
+                       }else{
+                           p.adicionaPontos(player2, p.verificaPontos("RealEnvido"));
+                       }
+                   }else{
+                       p.adicionaPontos(player2, p.verificaPontos("Envido"));
+                   }
+               }else{
+                   if(envido == 3){
+                       int resposta_envido = JOptionPane.showOptionDialog(null, cartasPlayer1, player2.getNome() + " aceita a Falta Envido?", JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
+                       if(resposta_envido == 1){
+                           int envP1 = p.calculaEnvido(player1.listaCarta);
+                           int envP2 = p.calculaEnvido(player2.listaCarta);
+                           int g = p.venceChamada(player1.isMao(), envP1, envP2);
+                           if(g == 1){
+                               p.adicionaPontos(player1, p.verificaPontos("FaltaEnvido"));
+                           }else{
+                               p.adicionaPontos(player2, p.verificaPontos("FaltaEnvido"));
+                           }
+                       }else{
+                           p.adicionaPontos(player2, p.verificaPontos("Envido"));
+                       }
+                   }else{
+                       p.adicionaPontos(player2, p.verificaPontos("Nao"));
+                   }    
+               }
+           }
+    }
+    
+    public static void chamaFlor(Partida p, Player player1, Player player2){
+        Object[] confirmacao = {"Sim", "Não"};
+        String cartasPlayer1 = player1.listaCarta[0].toString() + player1.listaCarta[1].toString() + player1.listaCarta[2].toString();
+        Object[] cartasPlayer2 = {player2.listaCarta[0] != null ? player2.listaCarta[0].toString() : "", 
+                                  player2.listaCarta[1] != null ? player2.listaCarta[1].toString() : "", 
+                                  player2.listaCarta[2] != null ? player2.listaCarta[2].toString() : ""};
+        String[] florOpt = {"É bom...", "Contra Flor", "Flor ao Resto"};
+        int flor = JOptionPane.showOptionDialog(null, "FLOR", player2.getNome(), JOptionPane.YES_NO_OPTION, 0, null, florOpt, florOpt[0]);
+            if(flor == 0){
+                p.adicionaPontos(player1, p.verificaPontos("Flor"));
+            }else{
+                if(flor == 1){
+                    int resposta_flor = JOptionPane.showOptionDialog(null, "Aceita a Falta Envido?", player2.getNome(), JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
+                    if(resposta_flor == 0){
+                        int florP1 = p.calculaFlor(player1.listaCarta);
+                        int florP2 = p.calculaFlor(player2.listaCarta);
+                        int g = p.venceChamada(player1.isMao(), florP1, florP2);
+                        if(g == 1){
+                            p.adicionaPontos(player1, p.verificaPontos("ContraFlor"));
+                        }else{
+                            p.adicionaPontos(player2, p.verificaPontos("ContraFlor"));
+                        }
+                    }else{
+                        p.adicionaPontos(player2, p.verificaPontos("Flor"));
+                    }
+                }else{
+                    int resposta_flor = JOptionPane.showOptionDialog(null, "Aceita a Falta Envido?", player2.getNome(), JOptionPane.YES_NO_OPTION, 0, null, confirmacao, confirmacao[0]);
+                    if(resposta_flor == 0){
+                        int florP1 = p.calculaFlor(player1.listaCarta);
+                        int florP2 = p.calculaFlor(player2.listaCarta);
+                        int g = p.venceChamada(player1.isMao(), florP1, florP2);
+                        if(g == 1){
+                            p.adicionaPontos(player1, p.verificaPontos("FlorEoResto"));
+                        }else{
+                            p.adicionaPontos(player2, p.verificaPontos("FlorEoResto"));
+                        }
+                    }else{
+                        p.verificaPontos("Flor");
+                        p.adicionaPontos(player2, p.verificaPontos("Flor"));
+                    }
+                }
+            }
+    }
+    
     public static int chamaTruco(Partida p, int partidaRodada, Player player1, Player player2){
         String[] trucoOpt = {"Quero", "Não quero", "Retruco"};
             int truco = JOptionPane.showOptionDialog(null, "Aceita o Truco?", player2.getNome(), JOptionPane.YES_NO_OPTION, 0, null, trucoOpt, trucoOpt[0]);
@@ -335,7 +384,7 @@ public class Main{
                 if(truco == 2){
                     chamaValeQuatro(p, partidaRodada, player1, player2);
                 }else{
-                    return 0;
+                    return partidaRodada;
                 }
             }
             return partidaRodada;
@@ -347,7 +396,7 @@ public class Main{
             if(truco == 0){
                 partidaRodada++;
             }else{
-               return 0;
+               return partidaRodada;
             }
             return partidaRodada;
     }
@@ -361,6 +410,7 @@ public class Main{
             case 0:
                 if(player.listaCarta[0] != null){
                     Cartas f1 = player.listaCarta[0];
+                    System.out.println(player.getNome() + "\n" + player.listaCarta[0].toString());
                     player.listaCarta[0] = null;
                     return f1;
                 }else{
@@ -370,6 +420,7 @@ public class Main{
             case 1:
                 if(player.listaCarta[1] != null){
                     Cartas f2 = player.listaCarta[1];
+                    System.out.println(player.getNome() + "\n" + player.listaCarta[1].toString());
                     player.listaCarta[1] = null;
                     return f2;
                 }else{
@@ -379,6 +430,7 @@ public class Main{
             case 2:
                 if(player.listaCarta[2] != null){
                     Cartas f3 = player.listaCarta[2];
+                    System.out.println(player.getNome() + "\n" + player.listaCarta[2].toString());
                     player.listaCarta[2] = null;
                     return f3;
                 }else{
